d9785648633a8517c5bd336a65689855ee7c2a2b
osoyinas <osoyinas@gmail.com>
2024-01-04 23:18:35 +0100

    Style admin panel

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,14 @@
-import { PdfList } from "@/components/admin/PdfList";
-import  UploadPDF from "@/components/admin/UploadPDF";
+import { TopicsList } from '@/components/admin/TopicsList'
+import UploadPDF from '@/components/admin/UploadPDF'
 
-export default function page() {
+export default function page () {
   return (
-    <>
-    <h1>PDF upload</h1>
-    <PdfList/>
-    <UploadPDF/>
-    </>
-
-  );
+    <main className="w-[800px] mx-auto p-8">
+      <h1 className="text-4xl font-bold text-primary">Panel de administrador</h1>
+      <div className='flex flex-col gap-4'>
+      <TopicsList/>
+      <UploadPDF/>
+      </div>
+    </main>
+  )
 }
