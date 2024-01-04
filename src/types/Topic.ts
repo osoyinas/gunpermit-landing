99b421d43911313cd473e8e2b5746c61d4f64893
osoyinas <osoyinas@gmail.com>
2024-01-04 23:19:28 +0100

    Format with lintern

--- src/types/Topic.ts
+++ src/types/Topic.ts
@@ -1,17 +1,15 @@
-export interface TopicType {
-    id: number;
-    name: string;
-    subtopics: SubtopicType[];
-    questions: number;
-}
-
 export interface SubtopicType {
     id: number;
     name: string;
     questions: number;
     topic: number;
 }
-
+export interface TopicType {
+    id: number;
+    name: string;
+    subtopics: SubtopicType[];
+    questions: number;
+}
 export interface PdfType {
     id: number;
     name: string;
