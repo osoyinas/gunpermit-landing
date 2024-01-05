55fd7b05c1d90db48e4480ae666c6994a8a6ef93
osoyinas <osoyinas@gmail.com>
2024-01-05 23:16:30 +0100

    Add edit questions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,17 +1,99 @@
+/* eslint-disable no-undef */
+'use client'
 import { Question as QuestionType } from '@/types/Topic'
+import React, { useState } from 'react'
 
 export default function Question ({ question } : {question: QuestionType}) {
+  const [updatedQuestion, setUpdatedQuestion] = useState<QuestionType>(question)
+  const [edited, setEdited] = useState<boolean>(false)
+
+  const onQuestionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUpdatedQuestion((prevQuestion) => {
+      console.log(updatedQuestion)
+
+      if (prevQuestion.question === event.target.value) {
+        return prevQuestion
+      } else {
+        return {
+          ...prevQuestion,
+          question: event.target.value
+        }
+      }
+    }
+    )
+    setEdited(true)
+  }
+
+  const onAnswerChange = (answerIndex:number, newText: string) => {
+    setUpdatedQuestion((prevQuestion) => {
+      const updatedAnswers = prevQuestion.answers.map((answer, index) => {
+        if (index === answerIndex) {
+          return {
+            ...answer,
+            answer: newText
+          }
+        } else {
+          return answer
+        }
+      })
+      return {
+        ...prevQuestion,
+        answers: updatedAnswers
+      }
+    })
+    setEdited(true)
+  }
+
+  const handleCorrectChange = (newCorrectAnswerIndex:number) => {
+    setUpdatedQuestion((prevQuestion) => {
+      const updatedAnswers = prevQuestion.answers.map((answer, index) => {
+        if (index === newCorrectAnswerIndex) {
+          return {
+            ...answer,
+            is_true: !answer.is_true
+          }
+        } else {
+          return {
+            ...answer,
+            is_true: false
+          }
+        }
+      })
+      return {
+        ...prevQuestion,
+        answers: updatedAnswers
+      }
+    }
+    )
+    setEdited(true)
+  }
   return (
     <article className='py-8 px-12 pb-12 rounded-xl flex flex-col gap-8 relative z-30 border border-accent shadow-xl'>
-        <p className=' text-pretty rounded-xl text-base-content'>{question.question}</p>
+        <textarea className='text-pretty text-base-content bg-base-100' onChange={onQuestionChange} value={updatedQuestion.question}/>
         <ul className='flex flex-col gap-4'>
-            {question.answers.map((answer) => (
-                <li key={answer.answer} className=' p-2 px-4 rounded-xl bg-base-300 font-semibold'>{answer.answer}</li>
+            {updatedQuestion.answers.map((answer, index) => (
+                <li key={index} className='w-full flex items-center gap-4'>
+                    <input type="checkbox" className="checkbox checkbox-accent" checked={answer.is_true} onChange={() => {
+                      if (answer.is_true) return
+                      handleCorrectChange(index)
+                    }}/>
+                    <input className='bg-base-300 w-full p-2 px-4 rounded-xl font-semibold' type='text' value={answer.answer} onChange={
+                        (event: React.ChangeEvent<HTMLInputElement>) => {
+                          onAnswerChange(index, event.target.value)
+                        }
+                        }/>
+                </li>
             ))}
         </ul>
-        <footer className='absolute top-[0.8rem] left-[1.2rem]'>
-            <h3 className='font-semibold'>{question.id}</h3>
-        </footer>
+        {edited && (
+            <footer className='flex justify-between'>
+            <button className='btn btn-primary' onClick={() => { console.log(updatedQuestion) }}>Guardar</button>
+            <button className='btn btn-error' onClick={() => {
+              setUpdatedQuestion(question)
+              setEdited(false)
+            }}>Deshacer</button>
+            </footer>
+        )}
     </article>
   )
 }
