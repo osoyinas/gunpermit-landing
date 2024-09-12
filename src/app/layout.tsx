88ad14f7c7be422d47d2f1b9b4ecc07486a3a8a8
osoyinas <osoyinas@gmail.com>
2024-09-13 01:08:06 +0200

    Add shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import React from 'react'
 import { Navbar } from '@/components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tests del permiso de armas',
@@ -14,13 +12,20 @@ export const metadata: Metadata = {
 export default function RootLayout ({
   children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="es" className='bg-base-100 text-base-content'>
-      <body className={`${inter.className} md:w-[800px] w-[100%] mx-auto`}>
-        <Navbar />
-        {children}
+    <html lang="es">
+      <body className='min-h-screen'>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import { ModeToggle } from './ui/ModeToggle'
 
 export function Navbar () {
   return (
-<nav className="navbar w-full m-0 sticky top-0 z-50 bg-black text-white">
+<nav className="navbar w-full m-0 sticky top-0 z-50">
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -18,6 +19,7 @@ export function Navbar () {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
     </button>
   </div>
+  <ModeToggle />
 </nav>
   )
 }
