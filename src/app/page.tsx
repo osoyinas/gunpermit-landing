14de615a656f18c245530a7c6d94157abfa83092
osoyinas <osoyinas@gmail.com>
2024-09-16 12:15:07 +0200

    Comment previus code to dont lose it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 'use client'
 
 import { DashboardComponent } from '@/components/dashboard'
-import { Button } from '@/components/ui/button'
-import { useLogout } from '@/hooks/api/auth/useLogout'
-import { useRouter } from 'next/navigation'
+// import { Button } from '@/components/ui/button'
+// import { useLogout } from '@/hooks/api/auth/useLogout'
+// import { useRouter } from 'next/navigation'
 
 export default function Home () {
-  const router = useRouter()
-  const { logout } = useLogout()
-  const handleLogin = async () => {
-    router.push('/auth/login')
-  }
-  const handleLogout = async () => {
-    await logout()
-    router.push('/auth/login')
-  }
+  // const router = useRouter()
+  // const { logout } = useLogout()
+  // const handleLogin = async () => {
+  //   router.push('/auth/login')
+  // }
+  // const handleLogout = async () => {
+  //   await logout()
+  //   router.push('/auth/login')
+  // }
   return (
     <>
       {/* <p>main page</p>
