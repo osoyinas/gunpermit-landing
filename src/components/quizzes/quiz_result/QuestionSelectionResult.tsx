a4e0bd534a44f261e0616f233c4d70faf509696d
osoyinas <osoyinas@gmail.com>
2024-10-14 18:14:47 +0200

    Add QuestionSelectionResult component for displaying quiz question selection results

---
+++ src/components/quizzes/quiz_result/QuestionSelectionResult.tsx
@@ -0,0 +1,47 @@
+import { Loading } from '@/components/Loading'
+import { useMakeQuizQuestions } from '@/hooks/make-quiz/useMakeQuizQuestions'
+import { useQuizNavigation } from '@/hooks/make-quiz/useQuizNavigation'
+import { useResponseQuiz } from '@/hooks/make-quiz/useResponseQuiz'
+import React from 'react'
+export function QuestionSelectionResult (): React.ReactElement {
+  const { quizResponse } = useResponseQuiz()
+
+  const { goToQuestion, actualQuestionIndex } = useQuizNavigation()
+  const { questions } = useMakeQuizQuestions()
+
+  const answers = quizResponse?.answers
+  if (!answers || !questions) {
+    return <Loading />
+  }
+
+  const selectedQuestionStyle = 'border-blue-500 border-2 text-white'
+  const correctQuestionStyle = 'bg-green-900 text-white'
+  const incorrectQuestionStyle = 'bg-red-900 text-white'
+  return (
+    <ul className="m-auto max-w-4xl w-full flex flex-wrap gap-2 justify-center">
+      {answers.map((quizAnswer, index) => {
+        const question = questions[index]
+        const correctAnswerIndex = question.answers.findIndex((answer) => answer.is_true)
+        const isCorrect = quizAnswer.answer === correctAnswerIndex
+        const isSelected = actualQuestionIndex === index
+        return (
+          <li
+            key={quizAnswer.question}
+            className={`h-12 w-12 border rounded-none md:rounded-2xl transition-colors ${
+              isCorrect ? correctQuestionStyle : incorrectQuestionStyle
+            } ${isSelected ? selectedQuestionStyle : ''}`}
+          >
+            <button
+              className="w-full h-full"
+              onClick={() => {
+                goToQuestion(quizAnswer.question)
+              }}
+            >
+              {index + 1}
+            </button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
