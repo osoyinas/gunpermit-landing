6a6c7e697b7041cefdd830ec366fc2ee09c9968f
osoyinas <osoyinas@gmail.com>
2024-10-14 16:36:32 +0200

    Add styles to question selection

--- src/components/quizzes/questions/QuestionSelection.tsx
+++ src/components/quizzes/questions/QuestionSelection.tsx
@@ -2,20 +2,32 @@ import { Loading } from '@/components/Loading'
 import { useQuizNavigation } from '@/hooks/make-quiz/useQuizNavigation'
 import { useResponseQuiz } from '@/hooks/make-quiz/useResponseQuiz'
 import React from 'react'
-export function QuestionSelection ():React.ReactElement {
+export function QuestionSelection (): React.ReactElement {
   const { quizResponse } = useResponseQuiz()
 
-  const { goToQuestion } = useQuizNavigation()
+  const { goToQuestion, actualQuestionIndex } = useQuizNavigation()
   const answers = quizResponse?.answers
   if (!answers) {
     return <Loading />
   }
-  return (<ul className="w-full flex flex-wrap gap-2">
-      {
-        answers.map((question, index) => (
-          <li key={question.question} className='h-12 w-12 border'>
+
+  const selectedQuestionStyle = 'border-blue-500 border-2 text-white'
+  const CompletedQuestionStyle = 'bg-gray-700 text-white'
+  const UnansweredQuestionStyle = ''
+  return (
+    <ul className="m-auto max-w-4xl w-full flex flex-wrap gap-2 justify-center">
+      {answers.map((question, index) => {
+        const isAnswered = question.answer !== undefined
+        const isSelected = actualQuestionIndex === index
+        return (
+          <li
+            key={question.question}
+            className={`h-12 w-12 border rounded-none md:rounded-2xl transition-colors ${
+              isAnswered ? CompletedQuestionStyle : UnansweredQuestionStyle
+            } ${isSelected ? selectedQuestionStyle : ''}`}
+          >
             <button
-              className='w-full h-full'
+              className="w-full h-full"
               onClick={() => {
                 goToQuestion(question.question)
               }}
@@ -23,9 +35,8 @@ export function QuestionSelection ():React.ReactElement {
               {index + 1}
             </button>
           </li>
-        ))
-      }
-
-        </ul>
+        )
+      })}
+    </ul>
   )
 }
