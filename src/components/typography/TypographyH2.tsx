7b308620946accab17d6fa512ea64713c5a338aa
osoyinas <osoyinas@gmail.com>
2024-10-15 00:01:37 +0200

    Refactor TypographyH2 and TypographyH3 components to accept a className prop

--- src/components/typography/TypographyH2.tsx
+++ src/components/typography/TypographyH2.tsx
@@ -4,7 +4,7 @@ interface TypographyH2Props extends HTMLAttributes<HTMLHeadingElement> {}
 
 export function TypographyH2 (props: TypographyH2Props) {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className={`scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 ${props.className}`}>
       {props.children}
     </h2>
   )
--- src/components/typography/TypographyH3.tsx
+++ src/components/typography/TypographyH3.tsx
@@ -4,7 +4,7 @@ interface TypographyH3Props extends HTMLAttributes<HTMLHeadingElement> {}
 
 export function TypographyH3 (props: TypographyH3Props) {
   return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+    <h3 className={`scroll-m-20 text-2xl font-semibold tracking-tight ${props.className}`}>
       {props.children}
     </h3>
   )
