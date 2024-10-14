f64b04a0a7c9f17723e9457942884c28865e6dfe
osoyinas <osoyinas@gmail.com>
2024-10-15 00:01:49 +0200

    Refactor AnimatedGenericQuestionCard to accept a className prop

--- src/components/quizzes/generics/AnimatedGenericQuestionCard.tsx
+++ src/components/quizzes/generics/AnimatedGenericQuestionCard.tsx
@@ -1,14 +1,14 @@
 import { AnimatePresence, motion } from 'framer-motion'
 import { ReactNode } from 'react'
 
-export function AnimatedGenericQuestionCard (props: { children: ReactNode }) {
+export function AnimatedGenericQuestionCard (props: { children: ReactNode, className?: string }) {
   const { children } = props
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.9 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
-      className="flex-grow flex flex-col md:justify-center md:items-center md:py-16"
+      className={`flex-grow flex flex-col md:justify-center md:items-center ${props.className}`}
     >
       <AnimatePresence mode="wait">{children}</AnimatePresence>
     </motion.div>
