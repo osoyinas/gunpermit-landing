63928e46b143b5d16c6a227ac833db0fc1a5ab48
osoyinas <osoyinas@gmail.com>
2024-10-15 22:11:32 +0200

    Refactor axios baseURL to use API_URI from env.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+import { API_URI } from './env'
 export const axiosDefaultInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: `${API_URI}/api/v1/`,
   timeout: 5000,
   withCredentials: true
 })
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,11 @@
 export const ENVIROMENT = process.env.NODE_ENV
+export const API_URI = process.env.API_URI
 
 if (!ENVIROMENT) {
   throw new Error('NODE_ENV not found')
+}
+if (!API_URI) {
+  throw new Error('API_URI not found')
 }
 
 export function isProduction () {
