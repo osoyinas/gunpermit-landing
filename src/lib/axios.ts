b39b86e2b6364f5c42c2fb6b24d3be0cc93da2d0
osoyinas <osoyinas@gmail.com>
2024-10-16 17:10:52 +0200

    Update timeout to 5005

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 import { API_URI } from './env'
 export const axiosDefaultInstance = axios.create({
   baseURL: `${API_URI}/api/v1/`,
-  timeout: 5000,
+  timeout: 5005,
   withCredentials: true
 })
