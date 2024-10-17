e321251ff76299fb7c6a4489614f81051a138e87
osoyinas <osoyinas@gmail.com>
2024-10-17 18:51:51 +0200

    Add useAssessments hook for fetching places and next assessment

---
+++ src/hooks/api/assessments/useAssessments.ts
@@ -0,0 +1,29 @@
+import { Result, Ok, Err } from 'ts-results'
+import useAxios from '@/hooks/useAxios'
+import { Assessment, Place } from '@/types/Assessments'
+
+export function useAssessments (): {
+    getPlaces: () => Promise<Result<Array<Place>, string>>;
+  getNextAssessment: (placeId: number) =>Promise<Result<Assessment, string>>;
+  } {
+  const axios = useAxios()
+
+  const getPlaces = async (): Promise<Result<Array<Place>, string>> => {
+    try {
+      const response = await axios.get('/places/')
+      return Ok(response.data)
+    } catch (error) {
+      return Err('Ha ocurrido un error al obtener los lugares')
+    }
+  }
+
+  const getNextAssessment = async (placeId:number): Promise<Result<Assessment, string>> => {
+    try {
+      const response = await axios.get(`/assessments/${placeId}/next/`)
+      return Ok(response.data)
+    } catch (error) {
+      return Err('Ha ocurrido un error al obtener el próximo examen')
+    }
+  }
+  return { getPlaces, getNextAssessment }
+}
