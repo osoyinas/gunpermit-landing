b207e51084940bb95adebce159c94b553d5af981
osoyinas <osoyinas@gmail.com>
2024-10-19 22:46:56 +0200

    Refactor useResponseQuiz hook for better readability and maintainability

--- src/hooks/make-quiz/useResponseQuiz.ts
+++ src/hooks/make-quiz/useResponseQuiz.ts
@@ -2,25 +2,48 @@ import { useMakeQuiz } from '@/hooks/make-quiz/useMakeQuiz'
 import { QuizAnswer, QuizResponse } from '@/types/Quizzes'
 
 export function useResponseQuiz (): {
-  quizResponse: QuizResponse | undefined,
-  updateQuizResponse: (questionId: number, answerIndex: number | undefined) => void,
-  getQuizResponse: (questionId:number) => QuizAnswer | undefined
+  quizResponse: QuizResponse | undefined;
+  updateQuizResponse: (
+    questionId: number,
+    answerIndex: number | undefined
+  ) => void;
+  getQuizResponse: (questionId: number) => QuizAnswer | undefined;
+  resetQuizResponse: () => void;
   } {
   const { quizResponse, setQuizResponse } = useMakeQuiz()
-
-  const updateQuizResponse = (questionId: number, answerIndex: number | undefined) => {
+  const { quiz } = useMakeQuiz()
+  const updateQuizResponse = (
+    questionId: number,
+    answerIndex: number | undefined
+  ) => {
     if (!quizResponse) return
 
-    const newAnswers = quizResponse.answers.map(
-      (answer) => answer.question === questionId
+    const newAnswers = quizResponse.answers.map((answer) =>
+      answer.question === questionId
         ? { ...answer, answer: answerIndex }
         : answer
     )
     setQuizResponse({ ...quizResponse, answers: newAnswers })
   }
-  const getQuizResponse = (questionId:number) => {
+  const getQuizResponse = (questionId: number): QuizAnswer | undefined => {
     if (!quizResponse) return
-    return quizResponse.answers.find((answer) => answer.question === questionId)
+    return quizResponse.answers.find(
+      (answer) => answer.question === questionId
+    )
   }
-  return { quizResponse: quizResponse ?? undefined, updateQuizResponse, getQuizResponse }
+  const resetQuizResponse = () => {
+    if (!quiz) return
+    setQuizResponse({
+      answers: quiz.questions.map((question) => ({
+        question: question.id,
+        answer: undefined
+      }))
+    })
+  }
+  return {
+    quizResponse: quizResponse ?? undefined,
+    updateQuizResponse,
+    getQuizResponse,
+    resetQuizResponse
+  }
 }
