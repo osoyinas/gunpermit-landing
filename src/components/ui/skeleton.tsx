5a7e918d3d28139d3a92cc36f8215d43278ec7c1
Pablo Piñas <osoyinas@gmail.com>
2024-10-22 15:53:11 +0200

    Fix linting build problems

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,9 +1,10 @@
 import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
 
 function Skeleton ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn('animate-pulse rounded-md bg-primary/10', className)}
