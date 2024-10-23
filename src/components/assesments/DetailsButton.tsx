ccb5b5f5d4f3ad81c7143a254a04e6affe5fd1f6
osoyinas <osoyinas@gmail.com>
2024-10-23 14:01:33 +0200

    Refactor DetailsButton component to use LinkButton instead of Button

--- src/components/assesments/DetailsButton.tsx
+++ src/components/assesments/DetailsButton.tsx
@@ -1,16 +1,15 @@
-import { useRouter } from 'next/navigation'
-import { Button } from '../ui/button'
+import { LinkButton } from '@components/ui/linkButton'
+import { AnchorHTMLAttributes } from 'react'
 
 const LINK = 'https://www.guardiacivil.es/documentos/pdfs/2024/ArmasyExplosivos/FECHAS_EXAMENES_PARA_OBTENCION_LICENCIAS_DE_ARMAS_DE_CAZA_27-02-2024.pdf'
-export function DetailsButton () {
-  const router = useRouter()
 
-  const handleClick = () => {
-    router.push(LINK)
-  }
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+}
+
+export function DetailsButton (props: Props) {
   return (
-    <Button className="w-full" onClick={handleClick}>
+    <LinkButton href={LINK} {...props}>
       Ver Detalles
-    </Button>
+    </LinkButton>
   )
 }
