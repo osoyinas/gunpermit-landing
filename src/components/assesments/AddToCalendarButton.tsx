b91c62d83ed1dcbac0aa17786440f33a1332efce
osoyinas <osoyinas@gmail.com>
2024-10-23 14:01:46 +0200

    Add AddToCalendarButton component

---
+++ src/components/assesments/AddToCalendarButton.tsx
@@ -0,0 +1,34 @@
+import React, { AnchorHTMLAttributes } from 'react'
+import { LinkButton } from '@components/ui/linkButton'
+
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+    title: string
+    startDate: Date
+    endDate?: Date
+    details: string
+    location: string
+}
+
+function AddToCalendarButton (props: Props):React.ReactNode {
+  const { title, startDate, endDate, details, location } = props
+
+  const formatGoogleCalendarDate = (date: Date): string => {
+    const year = date.getUTCFullYear()
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0')
+    const day = String(date.getUTCDate()).padStart(2, '0')
+
+    return `${year}${month}${day}`
+  }
+  const newStartDate = formatGoogleCalendarDate(startDate)
+  const newEndDate = endDate ? formatGoogleCalendarDate(endDate) : newStartDate
+
+  const googleCalendarUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${newStartDate}/${newEndDate}&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}`
+
+  return (
+    <LinkButton variant='secondary' href={googleCalendarUrl} target="_blank" rel="noopener noreferrer" {...props}>
+      Añadir a Google Calendar
+    </LinkButton>
+  )
+}
+
+export default AddToCalendarButton
