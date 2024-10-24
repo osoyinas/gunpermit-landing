97a2dc6e1469c281ff8f726a1aa25bca34195702
osoyinas <osoyinas@gmail.com>
2024-10-24 23:55:00 +0200

    Refactor Dashboard component to use QuickAccessCard for quick access links

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,40 +1,20 @@
 'use client'
 
 import * as React from 'react'
-import { BookOpenIcon, ClipboardIcon, TrophyIcon } from 'lucide-react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
+
 import { FullscreenContainer } from './ui/FullscreenContainer'
 import { NextAssessmentCard } from './dashboard/assesments/NextAssessmentCard'
 import { RecentResultsCard } from './dashboard/results/RecentResultsCard'
 import { PuntuationProgressCard } from './dashboard/stats/progress/PuntuationProgressCard'
 import { TopicPerformanceCard } from './dashboard/stats/topics/TopicPerformanceCard'
+import { QuickAccessCard } from './dashboard/quick_access/QuickAccessCard'
 
 export function Dashboard () {
   return (
     <FullscreenContainer className="flex flex-col max-w-7xl m-auto">
       <main className="flex-1  px-2 py-4 space-y-6">
         <div className="grid gap-4 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <Card>
-            <CardHeader>
-              <CardTitle>Acceso Rápido</CardTitle>
-              <CardDescription>Recursos de estudio y herramientas</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-              <Button className="w-full" variant="outline">
-                <BookOpenIcon className="mr-2 h-4 w-4" />
-                Material de Estudio
-              </Button>
-              <Button className="w-full bg-primary text-primary-foreground hover:bg-primary/90">
-                <ClipboardIcon className="mr-2 h-4 w-4" />
-                Prueba de Práctica
-              </Button>
-              <Button className="w-full" variant="outline">
-                <TrophyIcon className="mr-2 h-4 w-4" />
-                Mis Certificaciones
-              </Button>
-            </CardContent>
-          </Card>
+          <QuickAccessCard />
           <RecentResultsCard />
           <NextAssessmentCard />
           <div className='col-span-1 md:col-span-2'>
---
+++ src/components/dashboard/quick_access/QuickAccessCard.tsx
@@ -0,0 +1,27 @@
+import { CardContent, Card, CardDescription, CardHeader, CardTitle } from '@components/ui/card'
+import { BookOpenIcon, TrophyIcon } from 'lucide-react'
+import { LinkButton } from '@components/ui/linkButton'
+export function QuickAccessCard () {
+  return (
+        <Card>
+            <CardHeader>
+              <CardTitle>Acceso Rápido</CardTitle>
+              <CardDescription>Recursos de estudio y herramientas</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+            <LinkButton className="w-full" variant="default" href='/tests'>
+                <BookOpenIcon className="mr-2 h-4 w-4" />
+                Hacer tests
+              </LinkButton>
+              <LinkButton className="w-full" variant="outline" href='/resources'>
+                <BookOpenIcon className="mr-2 h-4 w-4" />
+                Material de Estudio
+              </LinkButton>
+              <LinkButton className="w-full" variant="outline" href='/progress'>
+                <TrophyIcon className="mr-2 h-4 w-4" />
+                Mi progreso
+              </LinkButton>
+            </CardContent>
+          </Card>
+  )
+}
