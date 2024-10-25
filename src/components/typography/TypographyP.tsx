31373921fc7aad88078033e654a2003e3a6a9d38
osoyinas <osoyinas@gmail.com>
2024-10-26 00:12:07 +0200

    Refactor TypographyP component to include custom class name support

--- src/components/typography/TypographyP.tsx
+++ src/components/typography/TypographyP.tsx
@@ -4,6 +4,6 @@ interface TypographyPProps extends HTMLAttributes<HTMLParagraphElement> {}
 
 export function TypographyP (props: TypographyPProps) {
   return (
-    <p className="leading-7 [&:not(:first-child)]:mt-6">{props.children}</p>
+    <p className={`leading-7 [&:not(:first-child)]:mt-6 ${props.className}`}>{props.children}</p>
   )
 }
