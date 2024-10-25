2b14ab79b886c3673ba2e5962b19ff84b5df8148
osoyinas <osoyinas@gmail.com>
2024-10-25 17:13:28 +0200

    Refactor FullscreenLoading component to accept children prop

--- src/components/FullscreenLoading.tsx
+++ src/components/FullscreenLoading.tsx
@@ -1,11 +1,13 @@
+import { ReactNode } from 'react'
 import { Loading } from './Loading'
 
-export function FullscreenLoading () {
+export function FullscreenLoading (props: { children: ReactNode }) {
   return (
 <div className=" text-foreground flex justify-center items-center" style={
         { height: 'calc(100vh - 4rem)' }
       }>
         <Loading />
+        <p className="ml-4 text-lg">{props.children}</p>
       </div>
   )
 }
