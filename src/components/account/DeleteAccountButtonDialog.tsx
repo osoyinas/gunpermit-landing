7902b37bf46a981bda05ac1f75a02cef87033ced
osoyinas <osoyinas@gmail.com>
2024-10-25 22:40:07 +0200

    Add DeleteAccountButtonDialog component

---
+++ src/components/account/DeleteAccountButtonDialog.tsx
@@ -0,0 +1,84 @@
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@components/ui/alert-dialog'
+import { Button } from '@components/ui/button'
+
+import useMediaQuery from '@hooks/useMediaQuery'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '@components/ui/drawer'
+import { useState } from 'react'
+
+const INFO = {
+  title: '¿Estás seguro de que quieres eliminar tu cuenta?',
+  description:
+      'Esta acción no se puede deshacer. Esto eliminará permanentemente tu cuenta y removerá tus datos de nuestros servidores.',
+  buttonLabel: 'Eliminar Cuenta'
+}
+
+export function DeleteAccountButtonDialog () {
+  const [open, setOpen] = useState(false)
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+  const handleDeleteAccount = () => {
+    // Logic to delete account
+    console.log('Deleting account')
+  }
+  if (isDesktop) {
+    return (
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="destructive">{INFO.buttonLabel}</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{INFO.title}</AlertDialogTitle>
+            <AlertDialogDescription>
+              {INFO.description}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <Button variant='destructive' onClick={handleDeleteAccount}>
+              {INFO.buttonLabel}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
+  }
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="destructive">Eliminar Cuenta</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>{INFO.title}</DrawerTitle>
+          <DrawerDescription>{INFO.description}</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter className="pt-2">
+            <Button variant="destructive" onClick={handleDeleteAccount}>
+                {INFO.buttonLabel}
+            </Button>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancelar</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
