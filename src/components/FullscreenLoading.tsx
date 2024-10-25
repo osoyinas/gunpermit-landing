15786ac5cda6d5ffd67ceeac810a98a7d1293618
osoyinas <osoyinas@gmail.com>
2024-10-26 00:54:54 +0200

    Refactor FullscreenLoading component to conditionally render children

--- src/components/FullscreenLoading.tsx
+++ src/components/FullscreenLoading.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from 'react'
 import { Loading } from './Loading'
 
-export function FullscreenLoading (props: { children: ReactNode }) {
+export function FullscreenLoading (props: { children?: ReactNode }) {
   return (
-<div className=" text-foreground flex justify-center items-center" style={
-        { height: 'calc(100vh - 4rem)' }
-      }>
-        <Loading />
-        <p className="ml-4 text-lg">{props.children}</p>
-      </div>
+    <div
+      className=" text-foreground flex justify-center items-center"
+      style={{ height: 'calc(100vh - 4rem)' }}
+    >
+      <Loading />
+      {props.children && <p className="ml-4 text-lg">{props.children}</p>}
+    </div>
   )
 }
