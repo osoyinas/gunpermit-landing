757b5083eb9a90e629f951e486f2ed63ea5b744c
osoyinas <osoyinas@gmail.com>
2024-10-26 11:29:27 +0200

    Refactor Navbar component to conditionally render content and account menus based on device type

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,19 +1,19 @@
 'use client'
-
 import useMediaQuery from '@hooks/useMediaQuery'
 import { DesktopAccountMenu } from './desktop/DesktopAccountMenu'
 import { MobileAccountMenu } from './mobile/MobileAccountMenu'
 import { DesktopContentMenu } from './desktop/DesktopContentMenu'
+import { MobileContentMenu } from './mobile/MobileContentMenu'
 
 export function Navbar () {
   const isDesktop = useMediaQuery('(min-width: 768px)')
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center m-auto">
-        {isDesktop && <DesktopContentMenu />}
+        {isDesktop ? <DesktopContentMenu /> : <MobileContentMenu />}
         <div className="flex-1"></div>
         {isDesktop ? <DesktopAccountMenu /> : <MobileAccountMenu />}
-        </div>
+      </div>
     </header>
   )
 }
