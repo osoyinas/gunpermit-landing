51f4b5e9de6e0b2214fd0c9740d4e62b09d07f99
osoyinas <osoyinas@gmail.com>
2024-10-26 12:54:16 +0200

    Remove console logs

--- src/components/dashboard/stats/progress/CustomDot.tsx
+++ src/components/dashboard/stats/progress/CustomDot.tsx
@@ -1,6 +1,5 @@
 export const CustomDot = (props: { cx: any; cy: any; payload: any }) => {
   const { cx, cy, payload } = props
-  console.log('payload', payload)
   const fillColor = payload.passed ? '#15803d' : '#7f1d1d'
 
   return <circle cx={cx} cy={cy} r={4} fill={fillColor} strokeWidth={1} />
--- src/hooks/useCopyUrl.ts
+++ src/hooks/useCopyUrl.ts
@@ -7,7 +7,6 @@ const useCopyURL = () => {
     }
     const url = window.location.href
     navigator.clipboard.writeText(url).then(() => {
-      console.log('URL copiada al portapapeles')
     }).catch(err => {
       console.error('Error al copiar la URL: ', err)
     })
