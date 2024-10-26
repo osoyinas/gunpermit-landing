aa88c4d8552bd4dc1cf8301268d60671c869ca89
osoyinas <osoyinas@gmail.com>
2024-10-26 16:58:41 +0200

    Refactor useTopics hook to use axiosInstance instead of axios for better clarity and consistency

--- src/hooks/api/quizzes/useTopics.ts
+++ src/hooks/api/quizzes/useTopics.ts
@@ -3,11 +3,11 @@ import useAxios from '@/hooks/useAxios'
 import { Topic } from '@/types/Topic'
 
 export function useTopics () {
-  const axios = useAxios()
+  const axiosInstance = useAxios()
 
   const getTopics = async (): Promise<Result<Array<Topic>, string>> => {
     try {
-      const response = await axios.get('/topics/')
+      const response = await axiosInstance.get('/topics/')
       return Ok(response.data)
     } catch (error) {
       return Err('Ha ocurrido un error al obtener los temas')
