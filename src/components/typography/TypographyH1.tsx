6b25a95ea6ebe51829c5abb55de5ce539a692ba4
osoyinas <osoyinas@gmail.com>
2024-10-28 22:30:21 +0100

    Refactor TypographyH1 component to accept className prop

--- src/components/typography/TypographyH1.tsx
+++ src/components/typography/TypographyH1.tsx
@@ -4,7 +4,7 @@ interface TypographyH1Props extends HTMLAttributes<HTMLHeadingElement> {}
 
 export function TypographyH1 (props: TypographyH1Props) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${props.className}`}>
       {props.children}
     </h1>
   )
