ab0d62f2a207a440acca6e0287d9f1ad42a91fdb
Pablo Piñas <osoyinas@gmail.com>
2024-10-28 12:47:54 +0200

    Refactor QuizSelection component to accept category prop

--- src/app/tests/categories/[category]/page.tsx
+++ src/app/tests/categories/[category]/page.tsx
@@ -3,6 +3,7 @@ import { QuizSelection } from '@/components/quizzes/QuizSelection'
 import { useRedirectIf } from '@/hooks/redirects/useRedirectIf'
 
 export default function Page ({ params }: { params: { category: string } }) {
+  const { category } = params
   useRedirectIf({ authenticated: false })
   return <QuizSelection category={category} />
 }
