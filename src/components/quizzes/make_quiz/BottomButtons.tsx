54c97e7eb9fd92a0b8ad1732447a777944c6e366
Pablo Piñas <osoyinas@gmail.com>
2024-10-29 17:53:43 +0100

    Add feedback when sending test

--- src/components/quizzes/make_quiz/BottomButtons.tsx
+++ src/components/quizzes/make_quiz/BottomButtons.tsx
@@ -2,8 +2,11 @@ import { Button } from '@/components/ui/button'
 import { useQuizzes } from '@/hooks/api/quizzes/useQuizzes'
 import { useMakeQuiz } from '@/hooks/make-quiz/useMakeQuiz'
 import { useResponseQuiz } from '@/hooks/make-quiz/useResponseQuiz'
+import { ErrorP } from '@components/ui/errorP'
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export function AnimatedBottomButtons () {
   return (
@@ -22,14 +25,17 @@ export function BottomButtons () {
   const { makeQuiz } = useQuizzes()
   const { quiz } = useMakeQuiz()
   const { quizResponse } = useResponseQuiz()
-  const disabled = quizResponse?.answers.some(question => question.answer == null)
   const router = useRouter()
-
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
   const handleCompleteQuiz = async () => {
+    setLoading(true)
     if (quiz && quizResponse) {
       const result = await makeQuiz(quiz.id, quizResponse)
       if (!result.ok) {
         console.error(result.val)
+        setError(result.val)
+        setLoading(false)
         return
       }
       const serializedQuizResponse = JSON.stringify(quizResponse)
@@ -37,12 +43,13 @@ export function BottomButtons () {
 
       const serializedQuizResult = JSON.stringify(result.val)
       const encodedQuizResult = encodeURIComponent(serializedQuizResult)
-
       router.push(`/tests/${quiz.id}/revision?quizResponse=${encodedQuizReponse}&quizResult=${encodedQuizResult}`)
     }
   }
 
+  const disabled = quizResponse?.answers.some(question => question.answer == null) || loading
   return (
+    <>
     <footer className='mx-auto px-2 md:px-0 py-8 max-w-4xl flex justify-between'>
       <div></div>
       <Button
@@ -51,8 +58,11 @@ export function BottomButtons () {
         variant="default"
         disabled={disabled}
       >
-        Enviar respuestas
+        {loading && <ReloadIcon className="w-6 h-6 mr-2 animate-spin" /> }
+        {loading ? 'Enviando...' : 'Enviar'}
       </Button>
     </footer>
+    {error && <ErrorP>{error}</ErrorP>}
+    </>
   )
 }
