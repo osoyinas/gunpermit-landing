33c8e5efda027a370051cd968f532f931669b213
osoyinas <osoyinas@gmail.com>
2024-10-31 19:34:32 +0100

    Add fullscreen loading component for session status in quiz page

--- src/app/tests/[id]/page.tsx
+++ src/app/tests/[id]/page.tsx
@@ -2,10 +2,13 @@
 
 import { MakeQuiz } from '@/components/quizzes/make_quiz/MakeQuiz'
 import { MakeQuizProvider } from '@/context/MakeQuizContext'
+import { FullscreenLoading } from '@components/FullscreenLoading'
+import { useSession } from 'next-auth/react'
 
 export default function Page ({ params }: { params: { id: number } }) {
   const { id } = params
-
+  const { status } = useSession()
+  if (status === 'loading') return <FullscreenLoading />
   return (
     <MakeQuizProvider>
       <MakeQuiz quizId={id} />
