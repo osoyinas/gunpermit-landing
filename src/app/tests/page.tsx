f5cae732a60037dd1afbbddc2457cb081d049028
osoyinas <osoyinas@gmail.com>
2024-10-31 19:32:28 +0100

    Add loading state with fullscreen loading component in Page

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 import { CategorySelection } from '@/components/quizzes/CategorySelection'
+import { FullscreenLoading } from '@components/FullscreenLoading'
+import { useSession } from 'next-auth/react'
 
 export default function Page () {
+  const { status } = useSession()
+  if (status === 'loading') return <FullscreenLoading />
   return (
     <CategorySelection />
   )
