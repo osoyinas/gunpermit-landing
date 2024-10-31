6d04f91d0157a49eb558bb285071ab41567a25e2
osoyinas <osoyinas@gmail.com>
2024-10-31 13:14:04 +0100

    Format with prettier

--- src/components/dashboard/quick_access/QuickAccessCard.tsx
+++ src/components/dashboard/quick_access/QuickAccessCard.tsx
@@ -1,27 +1,33 @@
-import { CardContent, Card, CardDescription, CardHeader, CardTitle } from '@components/ui/card'
+import {
+  CardContent,
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@components/ui/card'
 import { BookOpenIcon, TrophyIcon } from 'lucide-react'
 import { LinkButton } from '@components/ui/linkButton'
 export function QuickAccessCard () {
   return (
-        <Card>
-            <CardHeader>
-              <CardTitle>Acceso Rápido</CardTitle>
-              <CardDescription>Recursos de estudio y herramientas</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-            <LinkButton className="w-full" variant="default" href='/tests'>
-                <BookOpenIcon className="mr-2 h-4 w-4" />
-                Hacer tests
-              </LinkButton>
-              <LinkButton className="w-full" variant="outline" href='/resources'>
-                <BookOpenIcon className="mr-2 h-4 w-4" />
-                Material de Estudio
-              </LinkButton>
-              <LinkButton className="w-full" variant="outline" href='/progress'>
-                <TrophyIcon className="mr-2 h-4 w-4" />
-                Mi progreso
-              </LinkButton>
-            </CardContent>
-          </Card>
+    <Card>
+      <CardHeader>
+        <CardTitle>Acceso Rápido</CardTitle>
+        <CardDescription>Recursos de estudio y herramientas</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <LinkButton className="w-full" variant="default" href="/tests">
+          <BookOpenIcon className="mr-2 h-4 w-4" />
+          Hacer tests
+        </LinkButton>
+        <LinkButton className="w-full" variant="outline" href="/resources">
+          <BookOpenIcon className="mr-2 h-4 w-4" />
+          Material de Estudio
+        </LinkButton>
+        <LinkButton className="w-full" variant="outline" href="/progress">
+          <TrophyIcon className="mr-2 h-4 w-4" />
+          Mi progreso
+        </LinkButton>
+      </CardContent>
+    </Card>
   )
 }
