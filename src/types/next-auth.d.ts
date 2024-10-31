0d3a0e86a99e072efa858f2e1744c5250b021f63
osoyinas <osoyinas@gmail.com>
2024-10-31 18:45:05 +0100

    Enhance User and Session interfaces to include expires_in property

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,19 +4,20 @@ import NextAuth from 'next-auth'
 import { JWT } from 'next-auth/jwt'
 
 declare module 'next-auth' {
-    interface User {
-      first_name: string;
-      last_name?: string;
-      is_staff: boolean;
-      is_active: boolean;
-      is_superuser: boolean;
-      last_login: string;
-      date_joined: string;
+  interface User {
+    first_name: string;
+    last_name?: string;
+    is_staff: boolean;
+    is_active: boolean;
+    is_superuser: boolean;
+    last_login: string;
+    date_joined: string;
   }
   interface Session {
     user: User;
     access_token: string;
     token_type: 'Bearer';
+    expires_in: number;
   }
 }
 
@@ -25,5 +26,6 @@ declare module 'next-auth/jwt' {
     user: User;
     access_token: string;
     token_type: 'Bearer';
+    expires_in: number;
   }
 }
