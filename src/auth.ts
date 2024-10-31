479496a2fecdf4fead04ed53943b10c84c9223bb
osoyinas <osoyinas@gmail.com>
2024-10-31 18:44:26 +0100

    Fix autohrize method to login

--- src/auth.ts
+++ src/auth.ts
@@ -13,23 +13,32 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: { label: 'Password', type: 'password' }
       },
       async authorize (credentials: any) {
-        const response = await axios.post('/auth/login/', credentials)
+        const response = await axios.post('/auth/login/', credentials, {
+          validateStatus: () => true
+        })
         const data = response.data
-        if (response.status !== 200) throw new CredentialsSignin(data.detail)
+        if (response.status !== 200) {
+          console.log(response.statusText)
+          throw new CredentialsSignin()
+        }
         return data
       }
     })
   ],
-  callbacks: {
+  callbacks: { // 604800.0
     async jwt ({ token, user, account }) {
-      switch (account?.provider) {
-        case 'django':
-          // eslint-disable-next-line no-case-declarations
-          const credentialUser = user as any
-          token.access_token = credentialUser?.access_token
-          token.user = credentialUser?.user
-          token.token_type = 'Bearer'
-          break
+      if (account?.provider === 'django') {
+        const credentialUser = user as any
+        token.access_token = credentialUser?.access_token
+        token.user = credentialUser?.user
+        token.token_type = 'Bearer'
+        token.expires_in = credentialUser?.expires_in
+      }
+      console.log(token.expires_in)
+      console.log(Date.now())
+      if (Date.now() > token.expires_in) {
+        console.log('Token expired')
+        return null
       }
       return token
     },
@@ -37,10 +46,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       session.access_token = token.access_token
       session.user = token.user
       session.token_type = token.token_type
+      session.expires_in = token.expires_in
       return session
     }
   },
   session: {
     strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/auth/login'
   }
 })
