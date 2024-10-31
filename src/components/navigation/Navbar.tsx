8f481f8e17f108fed883c0b1fa7be8c41c41bba1
osoyinas <osoyinas@gmail.com>
2024-10-31 19:34:46 +0100

    Add skeleton to avoid jumps when navbar is not render

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -9,7 +9,15 @@ import { useSession } from 'next-auth/react'
 export function Navbar () {
   const isDesktop = useMediaQuery('(min-width: 768px)')
   const { status } = useSession()
-  if (status === 'loading') return null
+  if (status === 'loading') {
+    return (
+      <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 items-center m-auto">
+          <div className="flex-1"></div>
+        </div>
+      </header>
+    )
+  }
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
