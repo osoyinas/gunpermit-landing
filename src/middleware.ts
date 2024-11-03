b2c43c8884803d998768e526664807da6d7e2e8f
osoyinas <osoyinas@gmail.com>
2024-11-03 22:54:57 +0100

    Fix redirecting to dashboard when logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,13 @@ export default auth((req) => {
   const pathName = req.nextUrl.pathname
   const isProtected = protectedRoutes.some((route) => pathName.startsWith(route))
 
+  // Check if the user is authenticated and redirect to the dashboard if the user is trying to access the login page
+  if (req.auth && req.nextUrl.pathname === LOGIN_ROUTE) {
+    const newUrl = new URL('/dashboard', req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+
+  // If the route is not protected, continue
   if (!isProtected) return NextResponse.next()
 
   if (!req.auth && req.nextUrl.pathname !== LOGIN_ROUTE) {
