000445c26ed545b935c68d304190fe3df05257d6
osoyinas <osoyinas@gmail.com>
2024-11-03 19:29:25 +0100

    Remove console log from getTopics function

--- src/services/topics/getTopics.ts
+++ src/services/topics/getTopics.ts
@@ -5,7 +5,6 @@ import { Err, Ok, Result } from 'ts-results'
 export const getTopics = async (axiosInstance: Axios): Promise<Result<Array<Topic>, string>> => {
   try {
     const response = await axiosInstance.get('/topics/')
-    console.log(' GET TOPICS', response)
     return Ok(response.data)
   } catch (error) {
     return Err('Ha ocurrido un error al obtener los temas')
