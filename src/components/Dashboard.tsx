1bd25f96f360306a484c2baea5a232b24bcdf0c1
osoyinas <osoyinas@gmail.com>
2024-11-03 20:31:39 +0100

    Fix padding in dashboard and testContainer

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -15,8 +15,8 @@ export function Dashboard () {
   const { status } = useSession()
   if (status === 'loading') return <FullscreenLoading></FullscreenLoading>
   return (
-    <FullscreenContainer className="flex flex-col max-w-7xl m-auto py-12">
-      <main className="flex-1  px-2 py-4 space-y-6">
+    <FullscreenContainer className="flex flex-col max-w-7xl m-auto">
+      <main className="flex-1 px-2 py-4 space-y-6">
         <div className="grid gap-4 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <QuickAccessCard />
           <RecentResultsCard />
--- src/components/quizzes/TestsContainer.tsx
+++ src/components/quizzes/TestsContainer.tsx
@@ -6,7 +6,7 @@ interface FullscreenContainerProps extends HTMLAttributes<HTMLDivElement> {
 
 export function TestsContainer (props: FullscreenContainerProps) {
   return (
-    <main className='max-w-2xl px-2 md:p-0 md:mx-auto mt-8'{...props} style={
+    <main className='max-w-2xl px-5 md:px-0 md:mx-auto mt-8'{...props} style={
       { minHeight: 'calc(100dvh - 4rem)' }
     }>
       {props.children}
