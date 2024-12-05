7d879c1e74a39b9ea5b7b7e90fbe0a4f10028031
osoyinas <osoyinas@gmail.com>
2024-12-05 22:15:39 +0100

    Fix study resources

--- src/app/tests/[id]/study/page.tsx
+++ src/app/tests/[id]/study/page.tsx
@@ -2,10 +2,13 @@
 
 import { StudyQuiz } from '@components/quizzes/study_quiz/StudyQuiz'
 import { MakeQuizProvider } from '@/context/MakeQuizContext'
+import { useSession } from 'next-auth/react'
+import { FullscreenLoading } from '@components/FullscreenLoading'
 
 export default function Page ({ params }: { params: { id: number } }) {
   const { id } = params
-
+  const { status } = useSession()
+  if (status === 'loading') return <FullscreenLoading />
   return (
     <MakeQuizProvider>
       <StudyQuiz quizId={id} />
