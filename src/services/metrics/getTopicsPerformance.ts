b99c7983175ffc973b48a8aa7d65223daa5eb84b
Pablo Piñas <osoyinas@gmail.com>
2024-12-31 18:36:26 +0100

    Refactor getTopicsPerformance to fetch and structure topic performance data from the updated API endpoint

--- src/services/metrics/getTopicsPerformance.ts
+++ src/services/metrics/getTopicsPerformance.ts
@@ -1,12 +1,21 @@
-import { TopicPerformance } from '@/types/Metrics'
+import { TopicData, TopicPerformance } from '@/types/Metrics'
 import { AxiosInstance } from 'axios'
 import { Err, Ok, Result } from 'ts-results'
 export async function getTopicsPerformace (
   axios: AxiosInstance
 ): Promise<Result<Array<TopicPerformance>, string>> {
   try {
-    const response = await axios.get('metrics/results/topics/')
-    return Ok(response.data)
+    const response = await axios.get('metrics/results/topics/count/')
+
+    const topicsPerformance: Array<TopicPerformance> = []
+    for (const [topic, data] of Object.entries(response.data.topics)) {
+      topicsPerformance.push({
+        topic,
+        mark: (data as TopicData).correct,
+        full_mark: (data as TopicData).total
+      })
+    }
+    return Ok(topicsPerformance)
   } catch (error) {
     return Err(
       'Ha ocurrido un error al obtener los resultados de los exámenes'
