f5096783a883d6082553a8ab74638f8fbb4987cc
Pablo Piñas <osoyinas@gmail.com>
2025-04-18 20:38:48 +0200

    Add vercel analytics

--- src/components/navigation/NavbarLayout.tsx
+++ src/components/navigation/NavbarLayout.tsx
@@ -2,21 +2,22 @@ import { ReactNode } from 'react'
 import { Navbar } from './Navbar'
 import Footer from '@components/footer/Footer'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-export default function NavbarLayout (
-  { children }: {children: ReactNode}
-) {
+import { Analytics } from '@vercel/analytics/react'
+
+export default function NavbarLayout ({ children }: { children: ReactNode }) {
   return (
-    <div style={
-      {
+    <div
+      style={{
         display: 'grid',
         gridTemplateRows: 'auto 1fr auto',
         minHeight: '100dvh'
-      }
-    }>
+      }}
+    >
       <Navbar />
       {children}
       <Footer />
       <SpeedInsights />
+      <Analytics />
     </div>
   )
 }
